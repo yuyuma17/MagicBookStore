8a7e35f4f177cb7b114bbc2f4210b089bbb8d48c
Lacie <dark830917@gmail.com>
2019-11-04 10:35:28 +0800

    homePage storyBoard completed

--- MagicBookStore/HomePageViewController.swift
+++ MagicBookStore/HomePageViewController.swift
@@ -1,20 +1,18 @@
 //
-//  ViewController.swift
+//  HomePageViewController.swift
 //  MagicBookStore
 //
-//  Created by 黃士軒 on 2019/11/3.
+//  Created by 黃士軒 on 2019/11/4.
 //  Copyright © 2019 Lacie. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomePageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
 }
-
